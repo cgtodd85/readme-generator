49751ba4c3bf11c87fd28dd22e2996cb40ec64f6
Connor Todd <cgtodd85@gmail.com>
2021-07-11 19:52:00 -0400

    created test inquirer prompt

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("node is working");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "test",
+      message: "enter a test message",
+    },
+  ])
+  .then((data) => console.log(data));
