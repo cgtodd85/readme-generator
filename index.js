d1acf9f194e3af15b02bac770dc4a2751775150b
Connor Todd <cgtodd85@gmail.com>
2021-07-12 07:10:32 -0400

    updated prompts in index js

--- index.js
+++ index.js
@@ -5,8 +5,49 @@ inquirer
   .prompt([
     {
       type: "input",
-      name: "test",
-      message: "enter a test message",
+      name: "title",
+      message: "Please enter a title for your project: ",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Please enter a description, i.e. What motivated you? What problem does it solve? What did you learn? ",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please enter installation instructions: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter usage guidance: ",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Please enter additional ",
+    },
+    {
+      type: "input",
+      name: "",
+      message: "",
+    },
+    {
+      type: "input",
+      name: "",
+      message: "",
+    },
+    {
+      type: "input",
+      name: "",
+      message: "",
+    },
+    {
+      type: "input",
+      name: "",
+      message: "",
     },
   ])
   .then((data) => console.log(data));
