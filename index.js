dc06a5800f2fa3223fe2ad508c16ab8d67fc5ae6
Connor Todd <cgtodd85@gmail.com>
2021-07-11 20:05:04 -0400

    pseudo code to do list in index.js

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ inquirer
     },
   ])
   .then((data) => console.log(data));
+
+/* 
+  
+  To do:
+
+  Name of prompts
+  - title
+  - descrip
+  - table contents
+    - LINKS to click on that take you to sections of readme
+  - installation instruc
+  - usage
+ 
+  - contributing
+  - test instructions
+  - questions to ask me?
+    -add github username, link to user profile
+    - email address to reach you with questions
+  - license
+    - CHOICE from a LIST
+    - badge is added to top of readme with license info
+  
+write file to markdown .md and save
+  Final steps:
+  include screenshots
+  Screencastify demonstration
+
+
+  
+  
+  */
